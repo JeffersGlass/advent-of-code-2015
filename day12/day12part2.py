21bdc845838464bd24ef03e0fee05e4178486844
Jeff Glass <glass.jeffrey@gmail.com>
2024-12-05 09:53:45 -0600

    Day 12 part 2

--- day12/day12part2.py
+++ day12/day12part2.py
@@ -1,43 +1,22 @@
-import re
-import logging
+import json
+
+type JSON_PART = list | dict[str, JSON_PART] | str | int
 
 with open("day12/data.txt", "r") as f:
-    data = f.readlines()
+    jdata = json.loads(f.read())
 
-number_pattern = re.compile(r"-?\d+")
-red = re.compile("red")
-
-def process_line(line: str):
-    total = 0
-    red_count = 0
-    while True:
-        next_number = re.search(number_pattern, line)
-        if not next_number: break
-        next_number_pos = next_number.span()[0]
-        next_red = re.search(red, line)
-        next_red_pos = next_red.span()[0] if next_red else float("inf")
-
-        next_close = re.search("{", line)
-        next_close_pos = next_close.span()[0] if next_close else float("inf")
-
-        #number next
-        if next_number_pos < min(next_red_pos, next_close_pos):
-            if red_count <= 0: total += int(next_number.group())
-            print(f"{red_count=}, adding {next_number.group()} to total")
-            line = line[next_number.span()[1]:]
-        elif next_red_pos < min(next_number_pos, next_close_pos):
-            red_count += 1
-            print(f"Found red at position {next_red_pos}, {red_count=}")
-            line = line[next_red.span()[1]:]
-        elif next_close_pos< min(next_number_pos, next_red_pos):
-            red_count = max(red_count-1, 0) # THIS IS WRONG
-            line = line[next_close.span()[1]:]
-        else:
-            raise ValueError(f"No progress made, {next_number=} {next_red=} {next_close=}")
-    return total
-
-def test_process_line():
-    assert process_line("[1,2,3]") == 6
-    assert process_line('[1,{"c":"red","b":2},3]') == 4
-
-print(process_line('[1,{"c":"red","b":2},3]'))
+def sum_object(obj: JSON_PART) -> int:
+    if (t:= type(obj)) == int:
+        return obj
+    if t == str:
+        return 0
+    if t == list:
+        return sum(sum_object(i) for i in obj)
+    elif t == dict:
+        if "red" in obj.values(): return 0
+        return sum(sum_object(v) for v in obj.values())
+    else:
+        raise ValueError(f"Unknown type {t}")
+    
+if __name__ == "__main__":
+    print(sum_object(jdata))
